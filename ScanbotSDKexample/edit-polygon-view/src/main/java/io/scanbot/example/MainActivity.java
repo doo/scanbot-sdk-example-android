e786f0ec3d57e14224f8f8e55fbb5686cc231b87
Eduard Frank <eddy@scanbot.io>
2017-07-27 13:06:00 +0200

    Add cropping example with EditPolygonImageView

--- ScanbotSDKexample/edit-polygon-view/src/main/java/io/scanbot/example/MainActivity.java
+++ ScanbotSDKexample/edit-polygon-view/src/main/java/io/scanbot/example/MainActivity.java
@@ -7,9 +7,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.WindowCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import net.doo.snap.lib.detector.ContourDetector;
 import net.doo.snap.lib.detector.DetectionResult;
@@ -22,9 +23,13 @@ import java.util.List;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String POLYGON = "polygon";
 
     private EditPolygonImageView editPolygonView;
+    private MagnifierView magnifierView;
+    private Bitmap originalBitmap;
+    private ImageView resultImageView;
+    private Button cropButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,67 +41,95 @@ public class MainActivity extends AppCompatActivity {
 
         editPolygonView = (EditPolygonImageView) findViewById(R.id.polygonView);
         editPolygonView.setImageResource(R.drawable.test_receipt);
+        originalBitmap = ((BitmapDrawable) editPolygonView.getDrawable()).getBitmap();
 
-        MagnifierView magnifierView = (MagnifierView) findViewById(R.id.magnifier);
+        magnifierView = (MagnifierView) findViewById(R.id.magnifier);
         // MagifierView should be set up every time when editPolygonView is set with new image
         magnifierView.setupMagnifier(editPolygonView);
 
-        findViewById(R.id.get_polygon).setOnClickListener(new View.OnClickListener() {
+        resultImageView = (ImageView) findViewById(R.id.resultImageView);
+        resultImageView.setVisibility(View.GONE);
+
+        cropButton = (Button) findViewById(R.id.cropButton);
+        cropButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Call editPolygonView.getPolygon() to get new selected polygon.
-                Log.i("EDIT_POLYGON_EXAMPLE", editPolygonView.getPolygon().toString());
+                crop();
             }
         });
 
-        setPolygon(savedInstanceState);
+        backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backButton.setVisibility(View.GONE);
+                resultImageView.setVisibility(View.GONE);
 
-        new DetectLines().executeOnExecutor(Executors.newSingleThreadExecutor(), ((BitmapDrawable) editPolygonView.getDrawable()).getBitmap());
+                editPolygonView.setVisibility(View.VISIBLE);
+                cropButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        new InitImageViewTask().executeOnExecutor(Executors.newSingleThreadExecutor(), originalBitmap);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList(POLYGON, (ArrayList<PointF>) editPolygonView.getPolygon());
-    }
+    private void crop() {
+        // crop & warp image by selected polygon (editPolygonView.getPolygon())
+        final Bitmap documentImage = new ContourDetector().processImageF(
+                originalBitmap, editPolygonView.getPolygon(), ContourDetector.IMAGE_FILTER_NONE);
 
-    private void setPolygon(Bundle savedInstanceState) {
-        if (savedInstanceState == null) {
-            editPolygonView.setPolygon(EditPolygonImageView.DEFAULT_POLYGON);
-            return;
-        }
+        editPolygonView.setVisibility(View.GONE);
+        cropButton.setVisibility(View.GONE);
 
-        ArrayList<PointF> polygon = savedInstanceState.getParcelableArrayList(POLYGON);
-        editPolygonView.setPolygon(polygon);
+        resultImageView.setImageBitmap(documentImage);
+        resultImageView.setVisibility(View.VISIBLE);
+        backButton.setVisibility(View.VISIBLE);
     }
 
     /**
-     * Detects horizontal and vertical lines of the polygon
+     * Detects horizontal and vertical lines and polygon of the given bitmap image.
+     * Initializes EditPolygonImageView with detected lines and polygon.
      */
-    class DetectLines extends AsyncTask<Bitmap, Void, Pair<List<Line2D>, List<Line2D>>> {
+    class InitImageViewTask extends AsyncTask<Bitmap, Void, InitImageResult> {
 
         @Override
-        protected Pair<List<Line2D>, List<Line2D>> doInBackground(Bitmap... params) {
+        protected InitImageResult doInBackground(Bitmap... params) {
             Bitmap image = params[0];
             ContourDetector detector = new ContourDetector();
             final DetectionResult detectionResult = detector.detect(image);
+            Pair<List<Line2D>, List<Line2D>> linesPair = null;
+            List<PointF> polygon = new ArrayList<>(EditPolygonImageView.DEFAULT_POLYGON);
             switch (detectionResult) {
                 case OK:
                 case OK_BUT_BAD_ANGLES:
                 case OK_BUT_TOO_SMALL:
                 case OK_BUT_BAD_ASPECT_RATIO:
-                    return new Pair<>(detector.getHorizontalLines(), detector.getVerticalLines());
+                    linesPair = new Pair<>(detector.getHorizontalLines(), detector.getVerticalLines());
+                    polygon = detector.getPolygonF();
+                    break;
             }
 
-            return null;
+            return new InitImageResult(linesPair, polygon);
         }
 
         @Override
-        protected void onPostExecute(Pair<List<Line2D>, List<Line2D>> listListPair) {
-            super.onPostExecute(listListPair);
-            if (listListPair != null) {
-                editPolygonView.setLines(listListPair.first, listListPair.second);
+        protected void onPostExecute(final InitImageResult initImageResult) {
+            // set detected polygon and lines into EditPolygonImageView
+            editPolygonView.setPolygon(initImageResult.polygon);
+            if (initImageResult.linesPair != null) {
+                editPolygonView.setLines(initImageResult.linesPair.first, initImageResult.linesPair.second);
             }
         }
     }
+
+    class InitImageResult {
+        final Pair<List<Line2D>, List<Line2D>> linesPair;
+        final List<PointF> polygon;
+
+        InitImageResult(final Pair<List<Line2D>, List<Line2D>> linesPair, final List<PointF> polygon) {
+            this.linesPair = linesPair;
+            this.polygon = polygon;
+        }
+    }
+
 }
