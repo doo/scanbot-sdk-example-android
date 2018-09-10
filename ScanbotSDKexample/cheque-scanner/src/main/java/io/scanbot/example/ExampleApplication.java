5bf52681819082bc7e5f3dd565030127b42e8e88
Sergey Lebedev <sergey@scanbot.io>
2018-09-10 16:23:47 +0200

    Initial implementation of Cheque scanner

---
+++ ScanbotSDKexample/cheque-scanner/src/main/java/io/scanbot/example/ExampleApplication.java
@@ -0,0 +1,21 @@
+package io.scanbot.example;
+
+import android.app.Application;
+
+import io.scanbot.sdk.ScanbotSDKInitializer;
+
+/**
+ * {@link ScanbotSDKInitializer} should be called
+ * in {@code Application.onCreate()} method for RoboGuice modules initialization
+ */
+public class ExampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        new ScanbotSDKInitializer()
+                // TODO add your license
+                // .license(this, "YOUR_SCANBOT_SDK_LICENSE_KEY")
+                .initialize(this);
+        super.onCreate();
+    }
+}
