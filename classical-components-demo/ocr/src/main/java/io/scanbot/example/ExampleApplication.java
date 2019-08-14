b80fdba211333b1f2cae9b29bb0d26da7cd94c82
Eduard Frank <eddy@scanbot.io>
2019-08-14 18:19:22 +0200

    Update app IDs. Add license key hints.

--- classical-components-demo/ocr/src/main/java/io/scanbot/example/ExampleApplication.java
+++ classical-components-demo/ocr/src/main/java/io/scanbot/example/ExampleApplication.java
@@ -10,11 +10,21 @@ import io.scanbot.sdk.ScanbotSDKInitializer;
  */
 public class ExampleApplication extends Application {
 
+    /*
+     * TODO 1/2: Add the Scanbot SDK license key here.
+     * Please note: The Scanbot SDK will run without a license key for one minute per session!
+     * After the trial period is over all Scanbot SDK functions as well as the UI components will stop working
+     * or may be terminated. You can get an unrestricted "no-strings-attached" 30 day trial license key for free.
+     * Please submit the trial license form (https://scanbot.io/sdk/trial.html) on our website by using
+     * the app identifier "io.scanbot.example.sdk.android" of this example app.
+     */
+    private static final String LICENSE_KEY = "";
+
     @Override
     public void onCreate() {
         new ScanbotSDKInitializer()
-                // TODO add your license
-                // .license(this, "YOUR_SCANBOT_SDK_LICENSE_KEY")
+                // TODO 2/2: Enable the Scanbot SDK license key
+                // .license(this, LICENSE_KEY)
                 .prepareOCRLanguagesBlobs(true)
                 .initialize(this);
         super.onCreate();
--- ready-to-use-ui-demo/rtu_ui_components/src/main/java/io/scanbot/example/Application.kt
+++ ready-to-use-ui-demo/rtu_ui_components/src/main/java/io/scanbot/example/Application.kt
@@ -22,6 +22,14 @@ class Application : MultiDexApplication(), CoroutineScope {
 
 
     companion object {
+        /*
+         * TODO 1/2: Add the Scanbot SDK license key here.
+         * Please note: The Scanbot SDK will run without a license key for one minute per session!
+         * After the trial period is over all Scanbot SDK functions as well as the UI components will stop working
+         * or may be terminated. You can get an unrestricted "no-strings-attached" 30 day trial license key for free.
+         * Please submit the trial license form (https://scanbot.io/sdk/trial.html) on our website by using
+         * the app identifier "io.scanbot.example.sdk.rtu.android" of this example app.
+         */
         const val LICENSE_KEY = ""
     }
 
@@ -39,7 +47,8 @@ class Application : MultiDexApplication(), CoroutineScope {
                 .prepareOCRLanguagesBlobs(true)
                 .prepareMRZBlobs(true)
                 .preparePayFormBlobs(true)
-                //.license(this, LICENSE_KEY) // TODO Add the Scanbot SDK license key
+                // TODO 2/2: Enable the Scanbot SDK license key
+                // .license(this, LICENSE_KEY)
                 .initialize(this)
 
         launch {
