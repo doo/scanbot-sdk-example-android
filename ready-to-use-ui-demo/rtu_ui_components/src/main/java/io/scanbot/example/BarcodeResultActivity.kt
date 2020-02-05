9f2d990164ff2c703e2a033410576b15587c9059
Artem Kubiria <akubirya@gmail.com>
2020-02-05 11:15:26 +0100

    updated Barcode Scan RTU UI example

---
+++ ready-to-use-ui-demo/rtu_ui_components/src/main/java/io/scanbot/example/BarcodeResultActivity.kt
@@ -0,0 +1,69 @@
+package io.scanbot.example
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import io.scanbot.example.repository.BarcodeResultRepository
+import io.scanbot.sdk.barcode.entity.BarcodeScanningResult
+import kotlinx.android.synthetic.main.activity_barcode_result.*
+import kotlinx.android.synthetic.main.barcode_item.view.*
+import kotlinx.android.synthetic.main.snap_image_item.view.*
+import java.io.File
+
+class BarcodeResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode_result)
+        setSupportActionBar(toolbar)
+
+        showSnapImageIfExists(
+            BarcodeResultRepository.barcodeResultBundle?.previewPath
+                ?: BarcodeResultRepository.barcodeResultBundle?.imagePath
+        )
+
+        showLatestBarcodeResult(BarcodeResultRepository.barcodeResultBundle?.barcodeScanningResult)
+    }
+
+    private fun showSnapImageIfExists(imagePath: String?) {
+        imagePath?.let { imagePath ->
+            recognisedItems.addView(
+                layoutInflater.inflate(
+                    R.layout.snap_image_item,
+                    recognisedItems,
+                    false
+                )?.also {
+                    Picasso.with(this).load(File(imagePath)).into(it.snapImage)
+                })
+        }
+    }
+
+    private fun showLatestBarcodeResult(detectedBarcodes: BarcodeScanningResult?) {
+
+        detectedBarcodes?.let {
+            detectedBarcodes.barcodeItems.asSequence().map { item ->
+                layoutInflater.inflate(R.layout.barcode_item, recognisedItems, false)?.also {
+                    item.image?.let { bitmap ->
+                        it.image.setImageBitmap(bitmap)
+                    }
+                    it.barcodeFormat.text = item.barcodeFormat.name
+                    it.docFormat.text = item.barcodeDocumentFormat?.documentFormat
+                    it.docFormat.visibility = if (item.barcodeDocumentFormat != null) View.VISIBLE else View.GONE
+                    it.docText.text = item.text
+                    it.setOnClickListener {
+                        val intent = Intent(this, DetailedItemDataActivity::class.java)
+                        BarcodeResultRepository.selectedBarcodeItem = item
+                        startActivity(intent)
+                    }
+                }
+            }.forEach {
+                recognisedItems.addView(it)
+            }
+
+        }
+
+    }
+
+}
