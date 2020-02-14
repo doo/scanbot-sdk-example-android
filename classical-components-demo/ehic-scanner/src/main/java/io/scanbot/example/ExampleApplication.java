3485dc719cc27404b1bfe48561bdec14b2278e64
ildar.iskakov <ildar@scanbot.io>
2020-02-14 18:22:37 +0100

    Ehic scanner example

---
+++ classical-components-demo/ehic-scanner/src/main/java/io/scanbot/example/ExampleApplication.java
@@ -0,0 +1,49 @@
+package io.scanbot.example;
+
+import android.app.Application;
+import android.util.Log;
+
+import io.scanbot.sap.IScanbotSDKLicenseErrorHandler;
+import io.scanbot.sap.SdkFeature;
+import io.scanbot.sap.SdkLicenseInfo;
+import io.scanbot.sap.Status;
+import io.scanbot.sdk.ScanbotSDKInitializer;
+
+/**
+ * {@link ScanbotSDKInitializer} should be called
+ * in {@code Application.onCreate()} method for modules initialization
+ */
+public class ExampleApplication extends Application {
+
+    /*
+     * TODO 1/2: Add the Scanbot SDK license key here.
+     * Please note: The Scanbot SDK will run without a license key for one minute per session!
+     * After the trial period is over all Scanbot SDK functions as well as the UI components will stop working
+     * or may be terminated. You can get an unrestricted "no-strings-attached" 30 day trial license key for free.
+     * Please submit the trial license form (https://scanbot.io/sdk/trial.html) on our website by using
+     * the app identifier "io.scanbot.example.sdk.android" of this example app.
+     */
+    private static final String LICENSE_KEY = "";
+
+    @Override
+    public void onCreate() {
+        SdkLicenseInfo sdkLicenseInfo = new ScanbotSDKInitializer()
+                .licenceErrorHandler(new IScanbotSDKLicenseErrorHandler() {
+
+                    @Override
+                    public void handleLicenceStatusError(Status status, SdkFeature feature) {
+                        //handle license problem
+                        Log.d("ScanbotExample", "Status ${status.name} feature ${feature.name}");
+                    }
+                })
+                .prepareOCRLanguagesBlobs(true)
+                // TODO 2/2: Enable the Scanbot SDK license key
+                //.license(this, LICENSE_KEY)
+                .initialize(this);
+
+        //check scanbot sdk status here
+        Log.d("ScanbotExample", "Status " + sdkLicenseInfo.getStatus().name());
+
+        super.onCreate();
+    }
+}
---
+++ classical-components-demo/ehic-scanner/src/main/java/io/scanbot/example/MainActivity.java
@@ -0,0 +1,58 @@
+package io.scanbot.example;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import net.doo.snap.util.log.Logger;
+import net.doo.snap.util.log.LoggerProvider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final Logger logger = LoggerProvider.getLogger();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        askPermission();
+
+        Button liveScannerBtn = findViewById(R.id.live_scanner_btn);
+        liveScannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(EhicLiveDetectionActivity.newIntent(MainActivity.this));
+            }
+        });
+
+        Button stillImageScannerBtn = findViewById(R.id.still_image_detection_btn);
+        stillImageScannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(getApplicationContext(), EhicStillImageDetectionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void askPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                        999);
+            }
+        }
+    }
+}
