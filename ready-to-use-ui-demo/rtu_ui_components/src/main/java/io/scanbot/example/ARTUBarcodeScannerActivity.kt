4132a0b6c224633f22ff2e43a61fe352471cf290
Den Drobiazko <denys@scanbot.io>
2024-01-04 12:22:39 +0100

    Delete ScanbotTheme provider wrap, small adjustments

--- ready-to-use-ui-demo/rtu_ui_components/src/main/java/io/scanbot/example/ARTUBarcodeScannerActivity.kt
+++ ready-to-use-ui-demo/rtu_ui_components/src/main/java/io/scanbot/example/ARTUBarcodeScannerActivity.kt
@@ -6,37 +6,18 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.WindowCompat
 import io.scanbot.example.model.BarcodeResultBundle
 import io.scanbot.example.repository.BarcodeResultRepository
 import io.scanbot.sdk.ui_v2.barcode.BarcodeScannerView
-import io.scanbot.sdk.ui_v2.barcode.common.mappers.getName
-import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeItem
-import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeItemInfoPosition
-import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeItemMapper
-import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeMappedData
-import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeMappingResult
 import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeScannerConfiguration
-import io.scanbot.sdk.ui_v2.barcode.configuration.CollapsedVisibleHeight
-import io.scanbot.sdk.ui_v2.barcode.configuration.MultipleBarcodesScanningMode
-import io.scanbot.sdk.ui_v2.barcode.configuration.MultipleScanningMode
-import io.scanbot.sdk.ui_v2.common.CameraPreviewMode
-import io.scanbot.sdk.ui_v2.common.FinderCorneredStyle
-import io.scanbot.sdk.ui_v2.common.ScanbotColor
 import io.scanbot.sdk.ui_v2.common.StatusBarMode
-import io.scanbot.sdk.ui_v2.common.TopBarMode
 import io.scanbot.sdk.ui_v2.common.activity.AutoCancelTimeout
 import io.scanbot.sdk.ui_v2.common.activity.CanceledByUser
 import io.scanbot.sdk.ui_v2.common.activity.LicenseInvalid
-import io.scanbot.sdk.ui_v2.common.theme.LocalScanbotTheme
-import io.scanbot.sdk.ui_v2.common.theme.ScanbotTheme
-
 
 class ARTUBarcodeScannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +25,7 @@ class ARTUBarcodeScannerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_artu_barcode_scanner)
 
-        val cameraContainerView: ComposeView = findViewById(R.id.camera_container)
+        val cameraContainerView: ComposeView = findViewById(R.id.compose_container)
         cameraContainerView.apply {
             setContent {
                 val configuration = remember {
@@ -53,7 +34,7 @@ class ARTUBarcodeScannerActivity : AppCompatActivity() {
                     }
                 }
 
-                val statusBarHidden = configuration?.topBar?.statusBarMode == StatusBarMode.HIDDEN
+                val statusBarHidden = configuration.topBar.statusBarMode == StatusBarMode.HIDDEN
                 LaunchedEffect(key1 = true, block = {
                     if (statusBarHidden) {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -64,32 +45,28 @@ class ARTUBarcodeScannerActivity : AppCompatActivity() {
                     }
                 })
 
-                CompositionLocalProvider(LocalScanbotTheme provides ScanbotTheme(configuration.palette)) {
-                    Box {
-                        BarcodeScannerView(
-                            configuration = configuration,
-                            onBarcodeScanned = {
-                                BarcodeResultRepository.barcodeResultBundle = BarcodeResultBundle(it)
-                                val intent = Intent(context, BarcodeResultActivity::class.java)
-                                startActivity(intent)
-                                finish()
-                            },
-                            onBarcodeScannerClosed = {
-                                when (it) {
-                                    LicenseInvalid -> Toast.makeText(
-                                        context,
-                                        "License has expired!",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                    AutoCancelTimeout -> null // just close screen
-                                    CanceledByUser -> null // just close screen
-                                }
-                                finish()
-                            })
+                BarcodeScannerView(
+                    configuration = configuration,
+                    onBarcodeScanned = {
+                        BarcodeResultRepository.barcodeResultBundle = BarcodeResultBundle(it)
+                        val intent = Intent(context, BarcodeResultActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    },
+                    onBarcodeScannerClosed = {
+                        when (it) {
+                            LicenseInvalid -> Toast.makeText(
+                                context,
+                                "License has expired!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            AutoCancelTimeout -> Unit // just close screen (below)
+                            CanceledByUser -> Unit // just close screen (below)
+                        }
+                        finish()
                     }
-                }
+                )
             }
         }
     }
-
 }
