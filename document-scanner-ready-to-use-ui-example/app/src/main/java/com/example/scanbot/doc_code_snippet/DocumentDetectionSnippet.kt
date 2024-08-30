89be5d560bfc071ccf6f8fa2f2c372bed4784e45
Serhii Chaban <serhii@scanbot.io>
2024-08-30 14:03:26 +0200

    add cropping snippets

---
+++ document-scanner-ready-to-use-ui-example/app/src/main/java/com/example/scanbot/doc_code_snippet/DocumentDetectionSnippet.kt
@@ -0,0 +1,95 @@
+package com.example.scanbot.doc_code_snippet
+
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.scanbot.utils.getUrisFromGalleryResult
+import com.example.scanbot.utils.toBitmap
+import io.scanbot.page.PageImageSource
+import io.scanbot.sdk.ScanbotSDK
+import io.scanbot.sdk.docprocessing.Document
+import io.scanbot.sdk.ui_v2.common.ScanbotColor
+import io.scanbot.sdk.ui_v2.document.CroppingActivity
+import io.scanbot.sdk.ui_v2.document.configuration.CroppingConfiguration
+import io.scanbot.sdk.util.isDefault
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+private class DocumentDetectionSnippet : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //run this function on button click
+        importImagesFromLibrary()
+    }
+
+    private val scanbotSDK = ScanbotSDK(this@DocumentDetectionSnippet)
+    private val context = this
+
+    private val pictureForDocDetectionResult =
+        this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                activityResult.data?.let { imagePickerResult ->
+                    lifecycleScope.launch {
+                        withContext(Dispatchers.Default) {
+                            val document = scanbotSDK.documentApi.createDocument()
+                            getUrisFromGalleryResult(imagePickerResult)
+                                .asSequence() // process images one by one instead of collecting the whole list - less memory consumption
+                                .map { it.toBitmap(contentResolver) }
+                                .forEach { bitmap ->
+                                    if (bitmap == null) {
+                                        Log.e(
+                                            "StandaloneCropSnippet",
+                                            "Failed to load bitmap from URI"
+                                        )
+                                        return@forEach
+                                    }
+                                    document.addPage(bitmap)
+                                }
+                            startCropping(document)
+                        }
+                    }
+                }
+            }
+        }
+
+
+    val documentDetector = scanbotSDK.createContourDetector()
+    fun startCropping(document: Document) {
+        document.pages.forEach { page ->
+            val detectionResult = documentDetector.detect(page.originalImage!!)
+            if (detectionResult != null &&
+                detectionResult.polygonF.isNotEmpty() &&
+                !detectionResult.polygonF.isDefault()
+            ) {
+                page.apply(newPolygon = detectionResult.polygonF)
+                page.source = PageImageSource.IMPORTED
+            }
+        }
+    }
+
+
+    private fun importImagesFromLibrary() {
+        val imageIntent = Intent()
+        imageIntent.type = "image/*"
+        imageIntent.action = Intent.ACTION_GET_CONTENT
+        imageIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, false)
+        imageIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        imageIntent.putExtra(
+            Intent.EXTRA_MIME_TYPES,
+            arrayOf("image/jpeg", "image/png", "image/webp", "image/heic")
+        )
+        pictureForDocDetectionResult.launch(Intent.createChooser(imageIntent, "Select Picture"))
+    }
+
+}
+
