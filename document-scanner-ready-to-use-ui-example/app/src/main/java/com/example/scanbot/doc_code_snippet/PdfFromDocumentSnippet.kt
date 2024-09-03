0ad3bb4dbd31197e18e099c03118fea5c1e5dbfa
Serhii Chaban <serhii@scanbot.io>
2024-09-03 12:56:35 +0200

    add tiff and pdf snippets

---
+++ document-scanner-ready-to-use-ui-example/app/src/main/java/com/example/scanbot/doc_code_snippet/PdfFromDocumentSnippet.kt
@@ -0,0 +1,111 @@
+package com.example.scanbot.doc_code_snippet
+
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import androidx.lifecycle.lifecycleScope
+import com.example.scanbot.utils.getUrisFromGalleryResult
+import com.example.scanbot.utils.toBitmap
+import io.scanbot.pdf.model.PageDirection
+import io.scanbot.pdf.model.PageFit
+import io.scanbot.pdf.model.PageSize
+import io.scanbot.pdf.model.PdfAttributes
+import io.scanbot.pdf.model.PdfConfig
+import io.scanbot.pdf.model.ResamplingMethod
+import io.scanbot.sdk.ScanbotSDK
+import io.scanbot.sdk.docprocessing.Document
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+private class PdfFromDocumentSnippet : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //run this function on button click
+        importImagesFromLibrary()
+    }
+
+    private val scanbotSDK = ScanbotSDK(this@PdfFromDocumentSnippet)
+    private val context = this
+
+    private val pictureForDocDetectionResult =
+        this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                activityResult.data?.let { imagePickerResult ->
+                    lifecycleScope.launch {
+                        withContext(Dispatchers.Default) {
+                            val document = scanbotSDK.documentApi.createDocument()
+                            getUrisFromGalleryResult(imagePickerResult)
+                                .asSequence() // process images one by one instead of collecting the whole list - less memory consumption
+                                .map { it.toBitmap(contentResolver) }
+                                .forEach { bitmap ->
+                                    if (bitmap == null) {
+                                        Log.e(
+                                            "StandaloneCropSnippet",
+                                            "Failed to load bitmap from URI"
+                                        )
+                                        return@forEach
+                                    }
+                                    document.addPage(bitmap)
+                                }
+                            createPdfFromImages(document)
+                        }
+                    }
+                }
+            }
+        }
+
+    // Create a document detector instance
+    val pdfRenderer = scanbotSDK.createPdfRenderer()
+
+    fun createPdfFromImages(document: Document) {
+        val config = PdfConfig(
+            pdfAttributes = PdfAttributes(
+                author = "",
+                title = "",
+                subject = "",
+                keywords = "",
+                creator = ""
+            ),
+            pageSize = PageSize.A4,
+            pageDirection = PageDirection.AUTO,
+            dpi = 200,
+            jpegQuality = 100,
+            pageFit = PageFit.NONE,
+            resamplingMethod = ResamplingMethod.NONE,
+        )
+        val pdfRendered = pdfRenderer.render(
+            document,
+            config
+        )
+        val pdfFile = document.pdfUri.toFile()
+        if (pdfRendered && pdfFile.exists()) {
+            // Do something with the PDF file
+        } else {
+            Log.e("PdfFromDocumentSnippet", "Failed to create PDF")
+        }
+    }
+
+
+    private fun importImagesFromLibrary() {
+        val imageIntent = Intent()
+        imageIntent.type = "image/*"
+        imageIntent.action = Intent.ACTION_GET_CONTENT
+        imageIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, false)
+        imageIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        imageIntent.putExtra(
+            Intent.EXTRA_MIME_TYPES,
+            arrayOf("image/jpeg", "image/png", "image/webp", "image/heic")
+        )
+        pictureForDocDetectionResult.launch(Intent.createChooser(imageIntent, "Select Picture"))
+    }
+
+}
+
