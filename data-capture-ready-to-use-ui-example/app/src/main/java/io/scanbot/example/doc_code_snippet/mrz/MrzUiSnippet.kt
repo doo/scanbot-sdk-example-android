cf614b3743e45d9a55d52011213d8dae79bb03ce
Artem Kubiria <artem@scanbot.io>
2025-01-15 12:51:45 +0100

    Updated doc snippets

--- data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/mrz/MrzUiSnippet.kt
+++ data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/mrz/MrzUiSnippet.kt
@@ -1,5 +1,16 @@
 package io.scanbot.example.doc_code_snippet.mrz
 
+/*
+    NOTE: this snippet of code is to be used only as a part of the website documentation.
+    This code is not intended for any use outside of the support of documentation by Scanbot SDK GmbH employees.
+*/
+
+// NOTE for maintainers: whenever changing this code,
+// ensure that links using it are still pointing to valid lines!
+// Pay attention to imports adding/removal/sorting!
+// Page URLs using this code:
+// TODO: add URLs here
+
 import android.content.Context
 import android.content.Intent
 import android.widget.Button
@@ -24,7 +35,6 @@ fun initializeScanbotSDK(application: Application) {
         .initialize(application)
     // @EndTag("InitializeScanbotSDK")
 }
-
 
 class ResultApiCall : AppCompatActivity() {
 
--- data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/storage/StorageSnippet.kt
+++ data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/storage/StorageSnippet.kt
@@ -1,5 +1,16 @@
 package io.scanbot.example.doc_code_snippet.storage
 
+/*
+    NOTE: this snippet of code is to be used only as a part of the website documentation.
+    This code is not intended for any use outside of the support of documentation by Scanbot SDK GmbH employees.
+*/
+
+// NOTE for maintainers: whenever changing this code,
+// ensure that links using it are still pointing to valid lines!
+// Pay attention to imports adding/removal/sorting!
+// Page URLs using this code:
+// TODO: add URLs here
+
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
@@ -17,8 +28,6 @@ import io.scanbot.sdk.ui.view.licenseplate.*
 import io.scanbot.sdk.ui.view.licenseplate.configuration.LicensePlateScannerConfiguration
 import io.scanbot.sdk.ui.view.licenseplate.entity.LicensePlateScannerResult
 import java.io.File
-
-
 
 fun initializeCustomDir(application: Application) {
     // @Tag("Custom storage directory")
