67153c7935f91effa655c8f39015dbd92e9cd0a3
Serhii Chaban <serhii@scanbot.io>
2025-07-31 12:37:48 +0200

    add documentation to the snippet

--- data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/detailed_setup_guide/MockCameraSnippet.kt
+++ data-capture-ready-to-use-ui-example/app/src/main/java/io/scanbot/example/doc_code_snippet/detailed_setup_guide/MockCameraSnippet.kt
@@ -6,6 +6,20 @@ import io.scanbot.sdk.camera.ScanbotCameraViewConfigurationProvider
 
 class MockCameraSnippet {
     // @Tag("Mock Camera Initialization")
+    /*
+     * Initializes the Camera with a mock camera configuration.
+     * This is useful for testing purposes without requiring a physical camera.
+     *
+     * The mock camera will display a static image as the camera feed.
+     * Make sure to replace the image path with a valid one in your project.
+     *
+     * `MockCameraResourceProvider` - delegate that used for feeding camera with fake frames.
+     * `getFrameImagePath()` - should return path to the absolute path to the `File` in format without `file://` schema
+     * `getCapturedImagePath()` - should return path to the absolute path to the `File` in format without `file://` schema
+     * `showDebugImage` - if true, the mock camera will show `getFrameImagePath` file  as image preview.
+     * `tryHideFinderView` - if true, the finder view will be hidden.
+     * `tryHidePolygonView` - if true, the ar overlay polygon view will be hidden.
+     */
     fun initMockCamera() {
         ScanbotCameraViewConfigurationProvider.cameraViewType =
             CameraViewType.Mock(
