d71d36f93b34549b166e2d38b95c4f69bba1beb0
Adam Kvítek <adamkvitek@gmail.com>
2026-08-26 12:12:46 +0200

    docs: update trial license link to docs.scanbot.io/trial/
    The old scanbot.io/sdk/trial.html link is stale; point to the current
    trial page per Ildar.

--- classic-components-example/creditcard-scanner/src/main/java/io/scanbot/example/ExampleApplication.kt
+++ classic-components-example/creditcard-scanner/src/main/java/io/scanbot/example/ExampleApplication.kt
@@ -11,7 +11,7 @@ class ExampleApplication : Application() {
      * Please note: The Scanbot SDK will run without a license key for one minute per session!
      * After the trial period is over all Scanbot SDK functions as well as the UI components will stop working.
      * You can get an unrestricted "no-strings-attached" 7 day trial license key for free.
-     * Please submit the trial license form (https://scanbot.io/sdk/trial.html) on our website by using
+     * Please submit the trial license form (https://docs.scanbot.io/trial/) on our website by using
      * the app identifier "io.scanbot.example" of this example app.
      */
     val licenseKey = ""
